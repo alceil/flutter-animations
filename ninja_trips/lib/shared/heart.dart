2d9e8f246e6609545b2e64e6deb598d5ad1e57d0
alceil <ashishshajisrampickal@gmail.com>
2020-11-20 19:46:04 +0530

    added new animations

--- ninja_trips/lib/shared/heart.dart
+++ ninja_trips/lib/shared/heart.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class Heart extends StatefulWidget {
@@ -5,16 +7,33 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _colorAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: Duration(milliseconds: 200), vsync: this);
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red).animate(_controller);
+    _controller.forward();
+    _controller.addListener(() {
+      print(_controller.value);
+      print(_colorAnimation.value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
+    return AnimatedBuilder(
+      animation: _controller,
+      child: IconButton(
+        icon: Icon(
+          Icons.favorite,
+          color: _colorAnimation.value,
+          size: 30,
+        ),
+        onPressed: () {},
       ),
-      onPressed: () {},
     );
   }
 }
